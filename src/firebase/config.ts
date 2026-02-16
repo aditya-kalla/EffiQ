ea47b0605c32eee94b1138f00c887cb5002fe7d4
aditya-kalla <adityakalla.work@gmail.com>
2026-02-16 09:27:15 +0530

    Added the credentials

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,18 +1,22 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getFunctions } from 'firebase/functions';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyDn9mdrXzDo3VLgA0D8cpoikhAKCRayfjY",
+  authDomain: "effiq-b3e50.firebaseapp.com",
+  databaseURL: "https://effiq-b3e50-default-rtdb.firebaseio.com",
+  projectId: "effiq-b3e50",
+  storageBucket: "effiq-b3e50.firebasestorage.app",
+  messagingSenderId: "109863914318",
+  appId: "1:109863914318:web:8a82e9960f753302952c1f",
+  measurementId: "G-J06N577M83"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const functions = getFunctions(app);
+const analytics = getAnalytics(app);
